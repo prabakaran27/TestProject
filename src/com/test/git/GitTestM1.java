79e54fbde4908db725be222d3f89955f410ef775
prabakaran27 <mgrprabaran@gmail.com>
2014-11-11 10:23:01 +0000

    GitTestM1 added
    New file added

---
+++ src/com/test/git/GitTestM1.java
@@ -0,0 +1,20 @@
+package com.test.git;
+
+public class GitTestMO {
+	private String fileName;
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	private String author;
+	
+	
+}
