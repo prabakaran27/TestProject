bc33ec5fc8e3bdb6f59daeb91a54b78703fe470d
prabakaran27 <mgrprabaran@gmail.com>
2014-11-11 09:38:38 +0000

    added method
    update 1

--- src/com/test/git/util/GitTestUtil.java
+++ src/com/test/git/util/GitTestUtil.java
@@ -1,5 +1,11 @@
 package com.test.git.util;
 
 public class GitTestUtil {
+
+public String getDateAsString(){
+	String str = "";
+	System.out.println("Return date a String");
+	return str;
+}
 		
 }
